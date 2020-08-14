8360e6ff9b11c7986f11b8b26cca91e0110643bb
soyeong <sythegenuine@gmail.com>
2020-08-14 21:40:13 +0900

    initial

---
+++ otherworks/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def works(request):
+    return render(request, 'works.html')
+
+def community(request):
+    return render(request, 'community.html')
+
+def contact(request):
+    return render(request, 'contact.html')
