7b35ea28029699ba9df033a26af02704b83c0fdb
haeju98 <tracey1008@naver.com>
2020-08-15 14:49:45 +0900

    모델수정

--- otherworks/app/models.py
+++ otherworks/app/models.py
@@ -2,4 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# 웹툰
+class webtoon(models.Model):
+    idx = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=1000)
+    detaild_link = models.CharField(max_length=1000)
+    images = models.CharField(max_length=50000)
+    
 
+#웹소설
+class webnovel(models.Model):
+    idx = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=1000)
+    detaild_link = models.CharField(max_length=1000)
+    content = models.CharField(max_length=50000)
+
+
+#특가상품
+class price_deal(models.Model):
+    market = models.CharField(max_length=20)
+    idx = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    link = models.CharField(max_length=1000)
